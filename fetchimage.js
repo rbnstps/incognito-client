5a397dd6987ee7a2b27a1ce5fbee1d86d71c26a1
Ruben <0955301@hr.nl>
2021-06-04 16:47:15 +0200

    incognito client

---
+++ fetchimage.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const dataFetch = async function () {
+  const response = await fetch("http://85.214.45.48:8080/dataset");
+  const data = await response.json();
+
+  return data;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.getElementById("grid");
+
+  //   DIT BINNEN EEN FUNCTIE
+
+  dataFetch().then((images) => {
+    // images = data van de server
+
+    images.forEach((image) => {
+      // voor elke afbeelding binnen de afbeelding lijst: maak figure(element) aan
+      const figure = document.createElement("figure");
+      figure.classList = "gallery";
+      figure.classList = "image";
+      const imageElement = document.createElement("img");
+
+      imageElement.src = image.url;
+
+      figure.appendChild(imageElement);
+      grid.appendChild(figure);
+      // div.classList.add("gallery__img");
+    });
+  });
+});
